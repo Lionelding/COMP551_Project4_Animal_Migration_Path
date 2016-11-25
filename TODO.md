e5cb3f5dc37708fda1ef83934651d400496c7ac4
Keith Strickling <kstrickling@cloudops.com>
2016-11-25 13:12:47 -0500

    Added some more preprocessing

--- TODO.md
+++ TODO.md
@@ -5,8 +5,39 @@
     A: in general no
     Q: are there the same number of points for each animal?
     A: in general, no
-[ ] Does DTW require that each time series have measurements taken at the same times?
-    A: 
+[x] Does DTW require that each time series have measurements taken at the same times?
+    A: No, doesn't actually care about times or time interval between points
 
 ## Preprocessing
 [ ] handle this based on above research
+[ ] group by year
+
+## Sites
+Similarity Measures
+http://stats.stackexchange.com/questions/27861/similarity-measures-between-curves
+Clustering and classification examples
+https://github.com/alexminnaar/time-series-classification-and-clustering
+
+Procrustes - similar to dtw in that it will align curves
+https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.spatial.procrustes.html
+
+Questions we want to be able to answer:
+1. Comparing migrations between animals for a given year
+    - Can we spot outliers? Animals doing things differently?
+2. Comparing migrations for a single animal over many years
+    - Does the animal always take the same path?
+        - if so, how similar are the paths it takes? Identical? Roughly the same?
+        All the same except for a point or two? Can we find and explain outliers in the paths?
+3. Cluster a species based on migrations
+    - can we discover natural "herds" of animals? i.e. ones that take different
+    migrational paths or the same path but at different times?
+
+We need to be able to compare paths spatially and temporally.
+For spatial comparison, we can use dtw or Procrustes
+Seems we can also use dtw to detect a phase in paths!!!
+For temporal comparison, we have the following idea:
+
+Idea:
+- use interpolation to create additional time points so that each animal has locations for the same times
+- then use simple Euclidean distance between each pair of points
+- we can then cluster using 1-nn
--- preprocess.py
+++ preprocess.py
@@ -6,19 +6,57 @@ import csv
 import sys
 import numpy as np
 
-def read_data(fname, cols):
-    '''returns an array containing the data in the given cols of the file'''
-    data = []
-    with open(fname, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        # skip the header
-        reader.next()
-        for row in reader:
-            feature_vec = []
-            for col in cols:
-                feature_vec.append(row[col])
-            data.append(feature_vec)
-    return data
+''' Struture
+{
+    year: {
+        animal_id: {
+            data: [data points]
+        },
+        ...
+    },
+    ...
+}
+'''
 
-def preprocess(fname, cols):
-    return np.array(read_data(fname, cols))
+def get_col_index(col_titles, title):
+    for curr_title, i in enumerate(col_titles):
+        if curr_title == title:
+            return i
+    return -1
+
+def read_data(fname, cols, organize):
+    '''if organize=false, returns an array containing the data in the given cols of the file
+    else returns a dictionary with the above structure'''
+    if not organize:
+        data = []
+        with open(fname, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            # skip the header
+            reader.next()
+            for row in reader:
+                feature_vec = []
+                for col in cols:
+                    feature_vec.append(row[col])
+                data.append(feature_vec)
+        return data
+    else:
+        data = {}
+        with open(fname, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            # get the timestamp and individual-local-identifier columns
+            col_titles = reader.next()
+            timestamp_col = get_col_index(col_titles, 'timestamp')
+            if timestamp_col == -1:
+                print('Could not find column `timestamp`')
+                sys.exit(1)
+            ili_col = get_col_index(col_titles, 'individual-local-identifier')
+            if ili_col == -1:
+                print('Could not find column `individual-local-identifier`')
+                sys.exit(1)
+            # iterate row by row and create the data object
+
+            # for each set of datapoints, ensure that it is sorted
+            return data
+
+def preprocess(fname, cols, organize=True):
+    return np.array(read_data(fname, cols, organize))
